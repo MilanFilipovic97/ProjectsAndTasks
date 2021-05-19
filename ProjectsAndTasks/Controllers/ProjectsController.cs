00a714f03d72577fd74a7711fadbee4f4dd22ae1
milanfilipovicnp <milanfilipovicnp@hotmail.com>
2021-05-19 16:16:21 +0200

    first

---
+++ ProjectsAndTasks/Controllers/ProjectsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectsAndTasks.Models;
+
+namespace ProjectsAndTasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public ProjectsController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Projects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjectItems()
+        {
+            return await _context.ProjectItems.ToListAsync();
+        }
+
+        // GET: api/Projects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+            var project = await _context.ProjectItems.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        // PUT: api/Projects/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Projects
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+            _context.ProjectItems.Add(project);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProject), new { id = project.Id }, project);
+            //return CreatedAtAction("GetProject", new { id = project.Id }, project);
+        }
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Project>> DeleteProject(int id)
+        {
+            var project = await _context.ProjectItems.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProjectItems.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return project;
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.ProjectItems.Any(e => e.Id == id);
+        }
+    }
+}
